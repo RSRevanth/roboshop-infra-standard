87e1acc920914c2e742d5c7b59a929dee413ab47
Rayudu <Rayudu@gmail.com>
2024-09-12 11:16:28 +0530

    roboshop infra standard

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   public_subnet_cidr = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
-  Env = var.env
+  env = var.env
 
   #peering
   is_peering_required = true
